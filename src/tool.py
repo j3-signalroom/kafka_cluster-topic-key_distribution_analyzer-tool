2bd86914d1f18438e5b110da7ae9d626df370cc9
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-10-08 18:03:48 -0400

    Resolved #23.

--- src/tool.py
+++ src/tool.py
@@ -31,6 +31,34 @@ __status__     = "dev"
 logger = setup_logging()
 
 
+@st.cache_data(ttl=900, show_spinner="Loading Environment with Kafka Credentials...")
+def load_environment_with_kakfa_credentials() -> tuple[Dict, Dict, Dict, Dict]:
+    # Load environment variables from .env file
+    load_dotenv()
+ 
+    # Fetch environment variables non-credential configuration settings
+    try:
+        environment_filter = os.getenv("ENVIRONMENT_FILTER")
+        kafka_cluster_filter = os.getenv("KAFKA_CLUSTER_FILTER")
+        principal_id = os.getenv("PRINCIPAL_ID")
+        use_aws_secrets_manager = os.getenv("USE_AWS_SECRETS_MANAGER", DEFAULT_USE_AWS_SECRETS_MANAGER) == "True"
+    except Exception as e:
+        logging.error("THE APPLICATION FAILED TO READ CONFIGURATION SETTINGS BECAUSE OF THE FOLLOWING ERROR: %s", e)
+        return None, None, None
+    
+    # Fetch Confluent Cloud credentials from environment variable or AWS Secrets Manager
+    cc_credential = fetch_confluent_cloud_credential_via_env_file(use_aws_secrets_manager)
+    if not cc_credential:
+        return None, None, None, None
+
+    # Fetch Kafka credentials
+    # Read the Kafka Cluster credentials using Confluent Cloud API key
+    environments, kafka_clusters, kafka_credentials = fetch_kafka_credentials_via_confluent_cloud_api_key(principal_id, cc_credential, environment_filter, kafka_cluster_filter)
+    if not environments or not kafka_clusters or not kafka_credentials:
+        return None, None, None, None
+    else:
+        return cc_credential, environments, kafka_clusters, kafka_credentials
+
 def run_tests(kafka_cluster: Dict) -> None:
     """Run the Key Distribution and Data Skew tests.
 
@@ -77,7 +105,7 @@ def cleanup(cc_credential: Dict, kafka_credentials: Dict) -> None:
     iam_client = IamClient(iam_config=cc_credential)
 
     # Delete all the Kafka Cluster API keys created for each Kafka Cluster instance
-    for kafka_credential in kafka_credentials:
+    for kafka_credential in kafka_credentials.values():
         http_status_code, error_message = iam_client.delete_api_key(api_key=kafka_credential["sasl.username"])
         if http_status_code != HttpStatus.NO_CONTENT:
             logging.warning("FAILED TO DELETE KAFKA CLUSTER API KEY %s FOR KAFKA CLUSTER %s BECAUSE THE FOLLOWING ERROR OCCURRED: %s.", kafka_credential["sasl.username"], kafka_credential['kafka_cluster_id'], error_message)
@@ -93,46 +121,25 @@ def main():
     st.title("Key Distribution Analyzer Dashboard")
     st.write("This Analyzer Tool displays the result of the Key Distribution and Data Skew analysis.")
 
-    # Load environment variables from .env file
-    load_dotenv()
- 
-    # Fetch environment variables non-credential configuration settings
-    try:
-        environment_filter = os.getenv("ENVIRONMENT_FILTER")
-        kafka_cluster_filter = os.getenv("KAFKA_CLUSTER_FILTER")
-        principal_id = os.getenv("PRINCIPAL_ID")
-        use_aws_secrets_manager = os.getenv("USE_AWS_SECRETS_MANAGER", DEFAULT_USE_AWS_SECRETS_MANAGER) == "True"
-    except Exception as e:
-        logging.error("THE APPLICATION FAILED TO READ CONFIGURATION SETTINGS BECAUSE OF THE FOLLOWING ERROR: %s", e)
-        return
-    
-    # Fetch Confluent Cloud credentials from environment variable or AWS Secrets Manager
-    cc_credential = fetch_confluent_cloud_credential_via_env_file(use_aws_secrets_manager)
-    if not cc_credential:
-        return
-    
-    # Fetch Kafka credentials
-    # Read the Kafka Cluster credentials using Confluent Cloud API key
-    environments, kafka_clusters, kafka_credentials = fetch_kafka_credentials_via_confluent_cloud_api_key(principal_id, cc_credential, environment_filter, kafka_cluster_filter)
-    if not environments or not kafka_clusters or not kafka_credentials:
-        return
+    # --- Load the environment and Kafka credentials
+    cc_credential, environments, kafka_clusters, kafka_credentials = load_environment_with_kakfa_credentials()
     
     # --- Create and fill in the two dropdown boxes used to filter data in the app
     selected_environment = st.selectbox(
         index=0, 
         label='Choose the Environment:',
-        options=[environment.get("display_name") for environment in environments]
+        options=[environment.get("display_name") for environment in environments.values()]
     )
+    selected_environment_id = {environment.get("id") for environment in environments.values() if environment.get("display_name") == selected_environment}
     selected_kafka_cluster = st.selectbox(
         index=0,
         label="Choose the Environment's Kafka Cluster:",
-        options=[kafka_cluster.get("display_name") for kafka_cluster in kafka_clusters if kafka_cluster.get("environment_id") == next(environment.get("id") for environment in environments if environment.get("display_name") == selected_environment)]
+        options=[kafka_cluster.get("display_name") for kafka_cluster in kafka_clusters.values() if kafka_cluster.get("environment_id") in selected_environment_id]
     )
-    selected_kafka_credentials = [kafka_credential for kafka_credential in kafka_credentials if kafka_credential.get("kafka_cluster_id") == selected_kafka_cluster.get("id")]
-
+    selected_kafka_cluster_id = {kafka_cluster.get("id") for kafka_cluster in kafka_clusters.values() if kafka_cluster.get("display_name") == selected_kafka_cluster}
+    kafka_cluster_id = list(selected_kafka_cluster_id)[0]
     if st.button("Click Me"):
-        # This code runs when button is clicked
-        result = run_tests(selected_kafka_credentials[0])
+        result = run_tests(kafka_credentials[kafka_cluster_id])
         st.success(result)
         st.balloons()
 
